0d29e982337cf96d52137728fe252203d99c873b
FantomPonyansky <povelitelvselennou@mail.ru>
2018-02-25 11:05:28 +0300

    Function Create_Fucker_Sprite has been changed to Draw_Fucker and now nothing is working

--- graphics.cpp
+++ graphics.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 
-sf::Sprite Create_Fucker_sprite(std::string Fucker_Name)
+void Draw_Fucker(sf::RenderWindow window, std::string Fucker_Name)
 {
     sf::Image FuckerImage;
     FuckerImage.loadFromFile(Fucker_Name);
@@ -8,21 +8,20 @@ sf::Sprite Create_Fucker_sprite(std::string Fucker_Name)
     FuckerTexture.loadFromImage(FuckerImage);
     sf::Sprite FuckerSprite;
     FuckerSprite.setTexture(FuckerTexture);
-    FuckerSprite.setPosition(10, 10);
-    return FuckerSprite;
+    FuckerSprite.setPosition(0, 0);
+    window.draw(FuckerSprite);
 }
 
 void Create_Window()
 {
     unsigned int screen_width = 600;       //FIXME
     unsigned int screen_height = 600;      //FIXME
-    sf::RenderWindow window(sf::VideoMode(screen_width, screen_height), "My window", sf::Style::Close);
-    while (window.isOpen())
+    sf::RenderWindow MainWindow(sf::VideoMode(screen_width, screen_height), "My window", sf::Style::Close);
+    while (MainWindow.isOpen())
     {
         //Here must be a lot of drawing different fuckers and some events
-        window.clear();
-        sf::Sprite sprite = Create_Fucker_sprite("Bek.jpg");
-        window.draw(sprite);
-        window.display();
+        MainWindow.clear();
+        Draw_Fucker(MainWindow, "Bek.jpg");
+        MainWindow.display();
     }
 }
