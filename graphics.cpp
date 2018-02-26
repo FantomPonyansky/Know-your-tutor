3e9bffee599882f9a4f98012a119c668fb1c36b3
FantomPonyansky <povelitelvselennou@mail.ru>
2018-02-26 23:04:33 +0300

    Create_Window now displaying rules (not so) and can call Game_Graphics which still can't do anything

--- graphics.cpp
+++ graphics.cpp
@@ -1,4 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+
+sf::Event event;
 
 void Draw_Fucker(sf::RenderWindow& window, std::string Fucker_Name)
 //This function gets window and draw in it picture of fucker, whose name is also given
@@ -9,26 +13,91 @@ void Draw_Fucker(sf::RenderWindow& window, std::string Fucker_Name)
     FuckerTexture.loadFromImage(FuckerImage);
     sf::Sprite FuckerSprite;
     FuckerSprite.setTexture(FuckerTexture);
-    FuckerSprite.setPosition(200, 100);
+    FuckerSprite.setPosition(0, 100);
     window.draw(FuckerSprite);
 }
 
-void Game(sf::RenderWindow& window)
-//This is the main part of the game
+void Game_Graphics(sf::RenderWindow& window)
+//This is the main part of the game drawer. This function catches events, draw fuckers and so on.
 {
-    //FIXME
+    while (window.isOpen())
+    {
+        window.clear();
+        Draw_Fucker(window, "Bek.jpeg"); //FIXME this function must start first battle
+        window.display();
+        while (window.pollEvent(event)) {
+
+            switch (event.type) {
+
+                case sf::Event::Closed:
+                    //FIXME here must be sending event "Close" to main queue
+                    window.close();
+                    break;
+
+                case sf::Event::KeyPressed:
+
+                    if (event.key.code == sf::Keyboard::Y) {
+                        //FIXME here must be code, explaining what to do if user says "YES"
+                    }
+
+                    if (event.key.code == sf::Keyboard::N){
+                        //FIXME here must be code, explaining what to do if user says "NO"
+                    }
+            }
+        }
+    }
+    //FIXME Where is all events and all stuff?
 }
 
 void Create_Window()
 //This function only display rules and call game-function when start-key pressed and close if close-key pressed
 {
-    unsigned int screen_width = 600;       //FIXME
-    unsigned int screen_height = 600;      //FIXME
+    unsigned int screen_width = 400;
+    unsigned int screen_height = 700;
     sf::RenderWindow MainWindow(sf::VideoMode(screen_width, screen_height), "My window", sf::Style::Close);
-    while (MainWindow.isOpen())
-    {
+    sf::Text rules;
+    sf::Font font;
+    font.loadFromFile("Admiration Pains.ttf");
+    rules.setFont(font);
+    rules.setColor(sf::Color::Magenta);
+    rules.setCharacterSize(18);
+    rules.setPosition(5, 5);
+    std::string RulesInString = "Here must be rules"; //FIXME here must be reading rules from file
+    rules.setString(RulesInString);
+    while (MainWindow.isOpen()) {
+
         MainWindow.clear(sf::Color(0, 0, 0, 255));
-        Draw_Fucker(MainWindow, "Bek.jpg");
+        MainWindow.draw(rules);
         MainWindow.display();
+
+        while (MainWindow.pollEvent(event)) {
+
+            switch (event.type) {
+
+                case sf::Event::KeyPressed: {
+
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        //FIXME here must be sending event "Close" to main queue
+                        MainWindow.close();
+                    }
+
+                    if (event.key.code == sf::Keyboard::S) {
+                        //FIXME here must be sending event "Start" to main queue
+                        Game_Graphics(MainWindow);
+                    }
+                }
+
+                case sf::Event::Closed:
+                    //FIXME here must be sending event "Close" to main queue
+                    MainWindow.close();
+                    break;
+
+            }
+        }
     }
+}
+
+void Send_Event_To_Queue()
+{
+    //FIXME
 }
